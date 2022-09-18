4ef5795c9fb1b8f50d79ac2662796b60b14c7300
owenokeeffe <112406843+owenokeeffe@users.noreply.github.com>
2022-09-18 17:15:40 -0400

    Update UnitConverter.java

--- ojo4_lab3/src/ojo4_lab3/UnitConverter.java
+++ ojo4_lab3/src/ojo4_lab3/UnitConverter.java
@@ -1,4 +1,5 @@
 package ojo4_lab3;
+
 import javax.swing.JOptionPane;
 public class UnitConverter {
 
@@ -6,19 +7,61 @@ public class UnitConverter {
 
 		String userInput = JOptionPane.showInputDialog("Enter the measurement and unit.");
 		
+		userInput = userInput.toLowerCase();
+		
+		System.out.println(converter(userInput));
 	}
-
-	public static String converter(String userInput) {
-		
-		String[] convert = userInput.split(userInput);
-		
-		double conversionFactor = Double.parseDouble(convert[0]);
-		
-		if(convert[1].equalsIgnoreCase("cm")) {
-			double newMeasure = conversionFactor / 2.54;
-			return userInput + " = " + newMeasure + " in";
-		}
-		
+public static String converter (String input) {
+	
+	String strValue = input.substring(0, input.indexOf(' '));
+	double conversionVal = Double.parseDouble(strValue);
+	String unit = input.substring(input.indexOf(' ') + 1, input.length());
+	
+	if(unit.equalsIgnoreCase("cm")){
+		double newVal = conversionVal / 2.54;
+		return input + " = " + newVal + " in";
 	}
 	
+	if(unit.equalsIgnoreCase("in")){
+		double newVal = conversionVal * 2.54;
+		return input + " = " + newVal + " cm";
+	}
+	
+	if(unit.equalsIgnoreCase("m")){
+		double newVal = conversionVal * 1.094;
+		return input + " = " + newVal + " yd";
+	}
+	
+	if(unit.equalsIgnoreCase("yd")){
+		double newVal = conversionVal / 1.094;
+		return input + " = " + newVal + " m";
+	}
+	
+	if(unit.equalsIgnoreCase("oz")){
+		double newVal = conversionVal * 28.45;
+		return input + " = " + newVal + " gm";
+	}
+	
+	if(unit.equalsIgnoreCase("gm")){
+		double newVal = conversionVal / 28.45;
+		return input + " = " + newVal + " oz";
+	}
+	
+	if(unit.equalsIgnoreCase("lb")){
+		double newVal = conversionVal / 2.205;
+		return input + " = " + newVal + " kg";
+	}
+	
+	if(unit.equalsIgnoreCase("kg")){
+		double newVal = conversionVal * 2.205;
+		return input + " = " + newVal + " lb";
+	}
+	
+
+	return "";
 }
+		
+
+		
+	}
+
