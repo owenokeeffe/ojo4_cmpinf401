a8bdf3f2f40a802777e1d39741b1c8879066879a
owenokeeffe <112406843+owenokeeffe@users.noreply.github.com>
2022-10-28 10:20:37 -0400

    finished lab 7

--- ojo4_lab7/src/ojo4_lab7/lab7.java
+++ ojo4_lab7/src/ojo4_lab7/lab7.java
@@ -1,6 +1,7 @@
 package ojo4_lab7;
 import javax.swing.JOptionPane;
 
+import java.util.Arrays;
 import java.util.Random;
 public class lab7 {
 
@@ -14,18 +15,49 @@ public class lab7 {
 	for(int i = 0; i < nums.length; i++) {
 		nums[i] = random.nextInt(10);
 	}
-    max(nums);
+	System.out.println(Arrays.toString(nums));
+	System.out.println("The max is: "+ (max(nums)));
+	System.out.println("The minimum is: " + (min(nums)));
+	System.out.println("The sum is: " + (sum(nums)));
+	System.out.println("The average is: " + (ave(nums)));
+
 	}
 
 	public static double max(int[] nums) {
-		int max = 0;
-		for(int i = 0; i < nums.length; i++) {
-			if(nums[i] < nums[i+1]) {
-				nums[i] = max;
+		int max = nums[0];
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] > max) {
+				max = nums[i];
 			}
 		}
 		return max;
 	}
 	
+	public static double min(int[] nums) {
+		int min = nums[0];
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] < min) {
+				min = nums[i];
+			}
+		}
+		return min;
+	}
+	
+	public static double sum(int [] nums) {
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+		}
+		
+		return sum;
+	}
+	
+	public static double ave(int [] nums) {
+		double ave = 0;
+		
+		ave = ((sum(nums)) / nums.length);
+		
+		return ave;
+	}
 	
 }
